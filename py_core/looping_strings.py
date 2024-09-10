b9a4f97870381aaa4089ccc6e58475861264bd8a
zadvajr <danielzadva@gmail.com>
2024-09-10 21:27:22 +0100

    looping through string

---
+++ py_core/looping_strings.py
@@ -0,0 +1,11 @@
+#Python - since strings are treated as arrays in python
+# you can loop through a string and print each character in the string using for loop as follows
+
+full_name = "Daniel Zadva Jnr"
+
+for letter in full_name:
+    print(letter)
+
+#it will print each letter in the string "Daniel Zadva Jnr"
+
+#end
