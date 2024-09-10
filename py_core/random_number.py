dcad107ebd8e613471475517425c7cc9679d4c06
zadvajr <danielzadva@gmail.com>
2024-09-10 18:51:44 +0100

    Random Number

---
+++ py_core/random_number.py
@@ -0,0 +1,8 @@
+#Python - Random Number
+#Python does not have a random() function but has a module called random for generating a random number
+
+import random
+
+print(random.randrange(1, 10)) #prints random integer between 1 and 9 each time the program is run
+
+#end
