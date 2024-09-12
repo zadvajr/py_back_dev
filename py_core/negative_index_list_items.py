3e08364be4bdc35b5b047de002978994e991e9fe
zadvajr <danielzadva@gmail.com>
2024-09-12 22:27:38 +0100

    negative indexing in list

---
+++ py_core/negative_index_list_items.py
@@ -0,0 +1,13 @@
+#Access list items with negative index
+# if you access list item with a negative index, it will start from the end of the list
+# this trick can be used to access the last list item as follows
+
+fruits = ["Apple", "Banana", "Cherry", "Orange"] #list
+
+#to access last element 
+print(fruits[-1]) #prints: Orange
+
+#to access second to last element
+print(fruits[-2])
+
+#end
