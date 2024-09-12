d15f12e2e21ef255c071731e116cba112ede3c04
zadvajr <danielzadva@gmail.com>
2024-09-12 11:55:55 +0100

    capitalize() string method

---
+++ py_core/capitalize_string_method.py
@@ -0,0 +1,8 @@
+#String methods - capitalize()
+# capitalize() string methods capitalizes the first letter in first word of a sentence.
+
+#for example
+txt = "hello capitalize text! the first letter in the first hello will capitalize"
+print(txt.capitalize())
+
+#end
