150ff1f6ea0cc1fda4cd87aa299cfbbe4694cdf4
zadvajr <danielzadva@gmail.com>
2024-09-12 14:08:43 +0100

    foor division

---
+++ py_core/floor_division.py
@@ -0,0 +1,10 @@
+#Python floor division
+#this divides numbers without returning fractional part
+
+num1 = 5
+result = 5 // 2 #normal division suppose to return 2.5 byt with floor division you will get 2 as result
+
+print(result)
+
+
+#end
