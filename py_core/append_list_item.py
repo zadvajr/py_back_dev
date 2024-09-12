312d7e31b4b91f3fbf357a06cb28aee87a550292
zadvajr <danielzadva@gmail.com>
2024-09-12 23:24:44 +0100

    appending items to list

---
+++ py_core/append_list_item.py
@@ -0,0 +1,15 @@
+#append list item - to add an item to the end of the list  use append() method as follows
+
+fruits = ["Apple", "Banana"]
+
+print("Before appending: ", fruits)
+
+fruits.append("Cherry") # appends "Cheery" to the end of the list
+
+print("After appending: ", fruits)
+
+#you can also append more than one items at a time
+fruits.append("Orange", "Melon", "Kiwi", "Mango")
+print("After appending many items: ", fruits)
+
+#end
