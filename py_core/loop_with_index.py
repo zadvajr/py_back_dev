6dd93e1a17c4fa6428ae7209811068e1e8a7b700
zadvajr <danielzadva@gmail.com>
2024-09-13 17:42:34 +0100

    looping with index

---
+++ py_core/loop_with_index.py
@@ -0,0 +1,9 @@
+#Loop through list with index number
+
+fruits = ["Apple", "Banana", "Cherry", "Kiwi", "Lemon", "Orange", "Mango"]
+
+#loop with index
+for i in range(len(fruits)):
+    print(fruits[i])
+
+#end
