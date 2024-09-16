d25af9b7e61a5ed6068c7bc0fea0f28fbdb96ebb
zadvajr <danielzadva@gmail.com>
2024-09-17 00:15:50 +0100

    reversing a list

---
+++ py_core/reverse_list_order.py
@@ -0,0 +1,7 @@
+#Reversing Order of List - reverse() method can help if you want to reverse the order of the list regardless of its element
+fruits = ["banana", "Orange", "Kiwi", "cherry"]
+print("Before Reverse: ", fruits)
+fruits.reverse()
+print("After Reverse: ", fruits)
+
+#end
