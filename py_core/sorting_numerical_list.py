a59e52e4e7412412952909b1126d5336d3b7bdfe
zadvajr <danielzadva@gmail.com>
2024-09-16 23:01:45 +0100

    sorting numbered lists

---
+++ py_core/sorting_numerical_list.py
@@ -0,0 +1,8 @@
+#Sorting number list - using sort()
+
+users_age = [18, 34, 50, 25, 28]
+print("Users Age Before Sorting: ", users_age)
+users_age.sort()
+print("Users Age After Sorting: ", users_age)
+
+#end
