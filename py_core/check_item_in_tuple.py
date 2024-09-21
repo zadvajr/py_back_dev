6efb73f95ba9d2f95e653d03ddb7bfa40e0c6ec4
zadvajr <danielzadva@gmail.com>
2024-09-21 19:50:21 +0100

    check if item exist in tuple

---
+++ py_core/check_item_in_tuple.py
@@ -0,0 +1,7 @@
+#Check if item exist in a tuple using if and in keywords
+fruits = ("Apple", "Banana", "Cherry", "Lemon")
+print("Fruits: ", fruits)
+if "Apple" in fruits:
+    print("Yes, 'Apple' is in fruits")
+
+#end
