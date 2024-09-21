bfb192690cd27e6f6e25be54b24498eaba617bed
zadvajr <danielzadva@gmail.com>
2024-09-21 18:43:15 +0100

    length of tuple

---
+++ py_core/tuple_length.py
@@ -0,0 +1,7 @@
+#Tuple Length - this determines how many items a tuple has using the len() method
+
+thistuple = ("apple", "banana", "cherry", "lemon")
+print(thistuple)
+print("Leng of Tuple: ", len(thistuple))
+
+#end
