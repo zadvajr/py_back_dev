7c5d68354e47d11ecda3e7c47050437f3d83ca1c
zadvajr <danielzadva@gmail.com>
2024-09-21 18:38:20 +0100

    creating tuple

---
+++ py_core/tuples.py
@@ -0,0 +1,9 @@
+#Python Tuples - are used to store multiple items in a single variables
+# tuple is a collection which is ordered and unchangeable
+
+#creating tuple
+thistuple = ("Apple", "Banana", "Cherry")
+print(thistuple)
+print(type(thistuple))
+
+#end
