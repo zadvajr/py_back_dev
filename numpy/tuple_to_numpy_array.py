f82938a205df45259270a5ca22725fb5c259d511
zadvajr <danielzadva@gmail.com>
2024-09-23 00:11:34 +0100

    tuple to numpy array

---
+++ numpy/tuple_to_numpy_array.py
@@ -0,0 +1,10 @@
+#Tuple to numpy array - you can use tuple, list or any array like object to array() function to create numpy ndarray
+
+import numpy as np
+
+arr = np.array(("apple", "banana", "cherry"))
+
+print(arr)
+print(type(arr))
+
+#end
