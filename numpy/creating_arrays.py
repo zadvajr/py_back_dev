0c980ae7bcffdf8c2feab1248ef87f8e2cfc94ef
zadvajr <danielzadva@gmail.com>
2024-09-22 22:26:55 +0100

    creating numpy ndarray with array() function

---
+++ numpy/creating_arrays.py
@@ -0,0 +1,10 @@
+#Creating Arrays - numpy is used to work with array, the array object in numpy is called ndarray
+#we can create numpy ndarray objecy by using array() function
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6,7,8,9])
+print(arr)
+print(type(arr))
+
+#end
