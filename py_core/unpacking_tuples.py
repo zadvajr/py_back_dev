d6163d84fa59139b2b68845c37d8f1b8b23cdeea
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-14 14:23:16 +0100

    unpacking tuples

---
+++ py_core/unpacking_tuples.py
@@ -0,0 +1,16 @@
+"""No module"""
+
+#Unpacking tuples - when we create a tuple we normally assign value to it
+#this is called packing a tuple.
+#Packing tuple
+
+fruits = ("Aplle", "Banana", "Cherry")
+
+#unpacking tuples - means extracting values into respective variables
+(green, yellow, red) = fruits
+
+print(green) #Outputs - Apple
+print(yellow) #Outputs - Banana
+print(red) #Outputs - Cherry
+
+#end
