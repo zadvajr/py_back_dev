4f64a487157c487af6bbd7082f22afc2540ac247
zadvajr <danielzadva@gmail.com>
2024-10-15 00:16:29 +0100

    tuple methods

---
+++ py_core/tuple_methods.py
@@ -0,0 +1,9 @@
+"""Tuple Methos"""
+# There are two popular tuple methods count() and index()
+# The count() returns a number of times an item occurs in a tuple
+# While index() searches the tuple and returns the index or position of the item if found
+
+fruits = ("Apple", "Bananan", "Cherry", "Apple", "Orange", "Apple", "Lemon")
+
+print(fruits.count("Apple")) # Returns 3
+print(fruits.index("Cherry"))
