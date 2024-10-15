12e83d8ec37544ab2adb16a36b247ece25485cfc
zadvajr <danielzadva@gmail.com>
2024-10-15 22:59:18 +0100

    dictionary

---
+++ py_core/python_dictionary.py
@@ -0,0 +1,19 @@
+"""Python Dictionary"""
+#Dictionary is used to store data values in key:value pairs
+#it is a collection which is ordered, changeable and does not allow duplicates
+#from python 3.7 up dict becomes ordered
+
+#creating and printing a dictionary
+cars = {
+    "brand": "Toyota",
+    "model": "Camry",
+    "year": 2024
+}
+
+print(cars)
+
+
+
+
+
+#end
