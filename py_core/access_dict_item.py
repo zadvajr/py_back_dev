254db9b86303fbe9e49bafbdbd47ffa351afa798
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-16 16:55:55 +0100

    access dict items

---
+++ py_core/access_dict_item.py
@@ -0,0 +1,9 @@
+#Access Dictionary Items
+#You can access dictionary items by referring to its keys in square brackets or using the get() method
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
