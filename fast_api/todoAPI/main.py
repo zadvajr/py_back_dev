359df55853d82baca7fdc9ab182542a296887e4a
zadvajr <danielzadva@gmail.com>
2024-10-19 22:22:40 +0100

    finalised todoAPI

--- fast_api/todoAPI/main.py
+++ fast_api/todoAPI/main.py
@@ -26,4 +26,22 @@ def create_task(task: Task):
 def get_tasks():
     return tasks
 
-@app.get("/task/{task_id}")
+@app.get("/tasks/{task_id}", response_model=Task)
+def get_task(task_id: int):
+    if 0 <= task_id < len(tasks):
+        return tasks[task_id]
+    else:
+        raise HTTPException(status_code=404, detail="Task not found!")
+    
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, updated_task: Task):
+    if 0 <= task_id < len(tasks):
+        tasks[task_id] = update_task
+        return update_task
+    
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id: int):
+    if 0 <= task_id <= len(tasks):
+        deleted_task = tasks.pop(task_id)
+    else:
+        raise HTTPException(status_code=404, detail="Task not found!")
