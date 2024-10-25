5e4a07ceb41bfa17623270a3e50db3a242a5fd3e
zadvajr <danielzadva@gmail.com>
2024-10-25 23:46:27 +0100

    object methods

--- py_core/src/python_class_and_objects.py
+++ py_core/src/python_class_and_objects.py
@@ -39,6 +39,20 @@ print(p1.age) # Will print: 33
 # print(p1) # The output will be: <__main__.Car object at 0x7feb4ea5b050>
 
 #with __str__() 
+# class Car:
+#     def __init__(self, brand, model, year):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+    
+#     def __str__(self):
+#         return f"This car brand is: {self.brand} and it's Model is: {self.model}, {self.year} released."
+
+# p1 = Car("Toyota", "Camry", 2024)
+# print(p1)
+
+#object methods - objects can also contain methods. methods in objects are functions that belong to that object
+# let's create a method in the person class
 class Car:
     def __init__(self, brand, model, year):
         self.brand = brand
@@ -47,8 +61,22 @@ class Car:
     
     def __str__(self):
         return f"This car brand is: {self.brand} and it's Model is: {self.model}, {self.year} released."
+    
+    def can_speed(self):
+        print(f"{self.brand} can speed very well")
+
 
 p1 = Car("Toyota", "Camry", 2024)
 print(p1)
+p1.can_speed()
 
-#object methods
+#Modify object properties
+#You can modify properties on objects llike this:
+p1.brand = "Benz"
+p1.can_speed()
+
+#You can also delete object properties as follows:
+del p1.brand # accessing p1.brand will give an error
+# print(p1.brand)
+
+#The pass statement - class definition cannot be empty you can use pass keyword to prevent error
