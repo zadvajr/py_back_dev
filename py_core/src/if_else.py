e6eae4e5cd80320f7809ce4d45e5cd2f29af6504
zadvajr <danielzadva@gmail.com>
2024-10-26 14:13:32 +0100

    if else statement

--- py_core/src/if_else.py
+++ py_core/src/if_else.py
@@ -1,1 +1,20 @@
 #Python conditions and if else statement
+# Python supports the usual logical conditions from mathematics:
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+# These conditions can be used in several ways, most commonly in "if statements" and loops.
+
+# An "if statement" is written by using the if keyword.
+
+#Example
+#if statement
+a = 33 
+b = 200
+if b > a:
+    print("b is greater than a")
+
