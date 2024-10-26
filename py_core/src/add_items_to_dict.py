26a024d0e1f85587cfb90df8f250ef41c193d50b
zadvajr <danielzadva@gmail.com>
2024-10-26 11:47:11 +0100

    add items to dictionary

---
+++ py_core/src/add_items_to_dict.py
@@ -0,0 +1,15 @@
+#Adding items to python dictionary
+#Adding items to a dictionary is done by using a new index key and assigning a value to it.
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+#before
+print(car)
+
+car["color"] = "Red"
+print(car)
+print(car["color"])
