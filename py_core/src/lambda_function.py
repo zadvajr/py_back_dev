6db2642a2d27499cf6e27c88a36c0c11e88f55c2
zadvajr <danielzadva@gmail.com>
2024-10-26 23:57:05 +0100

    lambda function

---
+++ py_core/src/lambda_function.py
@@ -0,0 +1,7 @@
+#Python lambda function is a small anonymous functions
+#A lambda function can take any number of arguments but can only have a single expression
+#syntax - lambda arguments : expression
+# to convert that our sum() function to lambda function we do the following
+sum = lambda a,b:  a + b
+
+print(sum(5,10))
