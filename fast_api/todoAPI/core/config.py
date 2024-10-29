2931fe59da4d804dfd67aca91355a3401ea4eb9f
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-29 19:35:10 +0100

    added config files

---
+++ fast_api/todoAPI/core/config.py
@@ -0,0 +1,7 @@
+""" App Settings """
+class Settings:
+    """Settings Class"""
+    TITLE = "Todo API"
+    VERSION = "0.1.0"
+
+settings = Settings()
--- fast_api/todoAPI/main.py
+++ fast_api/todoAPI/main.py
@@ -1,47 +1,12 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
-import uvicorn
+""" modules import """
+from fastapi import FastAPI
+from core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.TITLE, version=settings.VERSION)
 
-tasks = []
-
-class Task(BaseModel):
-    title: str
-    description: str = ""
-
-@app.get("/", description="Welcome: This is a simple todo list app.")
+@app.get("/", description="To do API | Home Route")
 def home():
-    return "Welcome: This is a simple todo list app."
-
-@app.post("/tasks/")
-def create_task(task: Task):
-    tasks.append(task)
+    """ home route """
     return {
-        "Message": "Task added Successfully"
+        "message": "Welcome to Home Route"
     }
-
-@app.get("/tasks/", response_model=List[Task])
-def get_tasks():
-    return tasks
-
-@app.get("/tasks/{task_id}", response_model=Task)
-def get_task(task_id: int):
-    if 0 <= task_id < len(tasks):
-        return tasks[task_id]
-    else:
-        raise HTTPException(status_code=404, detail="Task not found!")
-    
-@app.put("/tasks/{task_id}", response_model=Task)
-def update_task(task_id: int, updated_task: Task):
-    if 0 <= task_id < len(tasks):
-        tasks[task_id] = update_task
-        return update_task
-    
-@app.delete("/tasks/{task_id}", response_model=Task)
-def delete_task(task_id: int):
-    if 0 <= task_id <= len(tasks):
-        deleted_task = tasks.pop(task_id)
-    else:
-        raise HTTPException(status_code=404, detail="Task not found!")
