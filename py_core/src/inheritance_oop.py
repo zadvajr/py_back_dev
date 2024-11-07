a139f8a851f7687adece1886c8ceed705b1f114d
zadvajr <danielzadva@gmail.com>
2024-11-07 21:26:06 +0100

    multiple inheritance

--- py_core/src/inheritance_oop.py
+++ py_core/src/inheritance_oop.py
@@ -10,3 +10,24 @@ f = FastAPI()
 print(f.is_cool)
 
 #Output: True
+
+#Multiple Inheritance
+class PaypalPayment:
+    payment_approved = True
+
+    def is_approved(self):
+        return self.payment_approved
+
+class StripePayment:
+    payment_approved = False
+
+    def is_approved(self):
+        return self.payment_approved
+
+class PaymentVerification(StripePayment, PaypalPayment):
+    pass
+
+payment = PaymentVerification
+print(payment.is_approved)
+
+#Output: False
