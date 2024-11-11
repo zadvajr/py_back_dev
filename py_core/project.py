dc020d8b35bd45a8ea2dcc716d6c1e636c8bfc10
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-11-11 21:31:59 +0100

    added project assignment

---
+++ py_core/project.py
@@ -0,0 +1,18 @@
+# Create a simple username and password authentication.
+# If the username and password specified are correct, then print
+# a success message, else print "You're are wrong!" five times.
+
+user_name = "zadvajr"
+password = "1059.dAz"
+
+trials = 3
+count = 0
+while count < trials:
+    username = input("Enter your username: ")
+    passw = input("Enter your password: ")
+    if username == user_name and passw == password:
+        print("You logged in successfully!")
+        break
+    else:
+        print("Wrong password!")
+        count += 1
